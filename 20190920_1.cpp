f58518b34805c55188dd3debcb09aab7c4ddfc75
GreatHua <25834024+zhangeh@users.noreply.github.com>
2019-09-20 12:02:37 +0800

    添加文件
    添加文件

---
+++ 20190920_1.cpp
@@ -0,0 +1,28 @@
+/**
+*@author zhangeh
+*@Date 2019/09/20
+*@desciption 计算字符串最后一个单词的长度，单词以空格隔开
+*@params hello world
+*@return 5
+*/
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//
+//    vector<string> wordsArray;
+//
+//    while(1){
+//        string str;
+//        cin >> str;
+//        if(str.find_last_of('\n')){
+//            break;
+//        }
+//        wordsArray.push_back(str);
+//    }
+//    cout << wordsArray.empty() ? 0 : wordsArray.back().length();
+//    return 0;
+//}
---
+++ 20190920_2.cpp
@@ -0,0 +1,28 @@
+/**
+*@author zhangeh
+*@Date 2019/09/20
+*@desciption 写出一个程序，接受一个由字母和数字组成的字符串，和一个字符，然后输出输入字符串中含有该字符的个数。不区分大小写
+*@params ABCDEF
+*@params A  第一行输入一个有字母和数字以及空格组成的字符串，第二行输入一个字符。
+*@return 输出输入字符串中含有该字符的个数。
+*/
+
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//    string target;
+//    char source;
+//    getline(cin,target);
+//    cin >> source;
+//    int number = 0;
+//    for(int i=0;i<target.length();i++){
+//        if(target[i]==source){
+//            number++;
+//        }
+//    }
+//    cout << number;
+//    return 0;
+//}
